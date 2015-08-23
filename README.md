43c52c86374864f5251578d0311d4c3b48bd0041
David <Ecutwxguan@163.com>
2015-08-23 14:51:40 +0800

    Demo without style done.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React-Practises
 A repository just for learning all about react.
+The music function lib is provided in https://github.com/goldfire/howler.js
---
+++ component/musicList.js
@@ -0,0 +1,50 @@
+/**
+ * Created by David on 8/23/15.
+ */
+var sound = new Howl({
+    urls: ['./music/javascript.mp3']
+}).play();
+
+var soundDataList = [
+    {
+        sound: new Howl({
+        urls: ['./music/javascript.mp3']
+        }),
+        displayContent:"javascript"
+    },
+    {
+        sound: new Howl({
+            urls: ['./music/demonstrate.mp3']
+        }),
+        displayContent:"demonstrate"
+    }
+];
+
+var MusicList = React.createClass({
+    render: function(){
+        var content = this.props.data.map(function(item){
+           var play = function(){
+               item.sound.play();
+           };
+           return (
+               [
+                   <div>
+                       <p>{item.displayContent}</p>
+                       <button onClick={play}>play</button>
+                   </div>
+               ]
+           )
+        });
+
+        return (
+            <div>
+                {content}
+            </div>
+        );
+    }
+});
+
+React.render(
+    <MusicList data={soundDataList}/>,
+    document.getElementById('example')
+);
