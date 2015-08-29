a18adbc38bad7c12aca55519b5d5445ba706808c
David <Ecutwxguan@163.com>
2015-08-29 21:28:58 +0800

    Just a little refactor

--- SoundPlayer/component/musicList.js
+++ SoundPlayer/component/musicList.js
@@ -1,26 +1,17 @@
 /**
  * Created by David on 8/23/15.
  */
-var soundDataList = [
-    {
-        sound: new Howl({
-        urls: ['./music/javascript.mp3']
-        }),
-        displayContent:"javascript"
-    },
-    {
-        sound: new Howl({
-            urls: ['./music/demonstrate.mp3']
-        }),
-        displayContent:"demonstrate"
-    }
-];
-
 var MusicList = React.createClass({
     render: function(){
         var content = this.props.data.map(function(item){
+           var soundContext;
            var play = function(){
-               item.sound.play();
+               if(soundContext == null){
+                   soundContext = new Howl({
+                       urls:item.urls
+                   });
+               }
+               soundContext.play();
            };
            return (
                [
--- SoundPlayer/component/renderManager.js
+++ SoundPlayer/component/renderManager.js
@@ -1,8 +1,22 @@
 /**
  * Created by David on 8/25/15.
  */
+var soundDataList = [
+    {
+        urls: ['./music/javascript.mp3'],
+        displayContent:"javascript"
+    },
+    {
+        urls: ['./music/demonstrate.mp3'],
+        displayContent:"demonstrate"
+    }
+];
+
 
 React.render(
-    <LanguageSign/>,
+    <div>
+        <LanguageSign />
+        <MusicList data={soundDataList}/>
+    </div>,
     document.getElementById('example')
 );
